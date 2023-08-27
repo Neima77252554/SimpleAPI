83cbddbf275a580b5a8c0bd95c187893395ef284
neimamohamedabdoulkader <neimamohamedabdoulkader@gmail.com>
2023-08-27 16:11:58 +0200

    Initial commit

---
+++ test/SimpleAPI.TEST/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace SimpleAPI.TEST;
+using Xunit;
+using SimpleAPI.Controllers;
+
+
+public class UnitTest1
+{
+  
+
+    [Fact]
+    public void Test1()
+    {
+
+    } 
+    WeatherForecastController controller = new  WeatherForecastController();
+    public void GetReturnsCoorectNumber(){
+        var returnValue= controller.Get(1);
+        Assert.Equal("something good is coming towards you", returnValue.Value);
+    }
+
+}
